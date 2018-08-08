563cde89ca7573c5aee84fa9f9ce55cbcbe165d5
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2018-08-08 17:57:05 +1000

    Update seeds

--- main.cpp
+++ main.cpp
@@ -357,16 +357,16 @@ extern "C" void* ThreadStats(void*) {
 }
 
 static const string mainnet_seeds[] = {
-  "vulcseed1.vulcanocrypto.com:62543",
-  "vulcseed2.vulcanocrypto.com:62543",
-  "vulcseed3.vulcanocrypto.com:62543",
-  "vulcseed4.vulcanocrypto.com:62543",
-  "vulcseed5.vulcanocrypto.com:62543",
-  "vulcseed1.vulcano.io:62543",
-  "vulcseed2.vulcano.io:62543",
-  "vulcseed3.vulcano.io:62543",
-  "vulcseed4.vulcano.io:62543",
-  "vulcseed5.vulcano.io:62543",
+  "138.201.245.247:62543",
+  "159.69.80.141:62543",
+  "159.69.84.71:62543",
+  "159.69.81.163:62543",
+  "159.69.83.104:62543",
+  "95.216.147.117:62543",
+  "95.216.148.73:62543",
+  "95.216.147.210:62543",
+  "159.69.49.144:62543",
+  "138.201.190.47:62543",
   ""
 };
 static const string testnet_seeds[] = {"test-node01.vulcanocrypto.com", "test-node01.vulcano.io", ""};

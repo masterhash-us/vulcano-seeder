7bea9265b4e79a0f1f2b09cacd34eea70b67ccd3
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2018-08-29 09:33:15 +1000

    Port

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 42133 : 52543;
+    return testnet ? 62133 : 62543;
 }
 
 //

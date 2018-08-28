9e63a86453f82b9d364dc21feefd86d20b342a1a
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2018-08-29 09:16:37 +1000

    CService ip

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -298,7 +298,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("ssus.tech", 52543, true);
+  CService ip("vulcanocrypto.com", 62543, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
